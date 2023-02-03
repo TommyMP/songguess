d36cc063645aeeb0e0829e607386053df2dbd394
LucaPaso04 <105647186+LucaPaso04@users.noreply.github.com>
2023-02-03 18:19:53 +0100

    Add files via upload

--- public/js/theme-switch.js
+++ public/js/theme-switch.js
@@ -1,16 +1,26 @@
 const themeSwitchBtn = document.getElementById("theme-switch-btn");
 const icon = document.getElementById("icon");
-const divInfo = document.getElementById("div-info-light");
-const divInput = document.getElementById("div-input-light");
+const info = document.getElementById("info-light");
+
+const playlist = document.getElementById("idPlaylist");
+const card = document.getElementById("idCard");
+const divTable = document.getElementById("idTable");
+const table = document.getElementById("users");
+
 
 themeSwitchBtn.addEventListener("click", function() {
-  divInfo.classList.toggle("div-info-dark");
-  divInput.classList.toggle("div-input-dark");
-  if (document.body.classList.contains("dark-theme")) {
-    icon.classList.remove("fa-moon");
-    icon.classList.add("fa-sun");
-  } else {
-    icon.classList.remove("fa-sun");
-    icon.classList.add("fa-moon");
-  }
+    info.classList.toggle("info-dark");
+    if (info.classList.contains("info-dark")) {
+      icon.classList.remove("fa-moon");
+      icon.classList.add("fa-sun");
+    } else {
+      icon.classList.remove("fa-sun");
+      icon.classList.add("fa-moon");
+    }
+    
+    playlist.classList.toggle("dark-theme");
+    card.classList.toggle("dark-theme");
+    icon.classList.toggle("text-light");
+    divTable.classList.toggle("text-light");
+    table.classList.toggle("text-light");
 });
